5de5fcd8ecfbcbc2a669b094217c771c7ae5d807
Yoshiya Hinosawa <stibium121@gmail.com>
2022-07-25 17:23:49 +0900

    test

--- require.ts
+++ require.ts
@@ -2,6 +2,6 @@ import { createRequire } from "https://deno.land/std@0.149.0/node/module.ts";
 import { serve } from "https://deno.land/std@0.149.0/http/server.ts";
 const require = createRequire(import.meta.url);
 //const a = require("./a.js");
-console.log(import.meta.url)
-console.log(await Deno.readTextFile("file:///src/a.js"))
+console.log(import.meta.url);
+console.log(await Deno.readTextFile(new URL("./a.js", import.meta.url)));
 serve(() => new Response("hello"));
